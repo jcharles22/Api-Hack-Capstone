10c22853d93ad5b59827170bfecda3e7ef257220
Jared Charles <jaredcharles22@gmail.com>
2019-04-10 19:53:29 -0400

    added syling to venue buttons

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 $(function (){
 let city;
+const catagories ={
+    food: '4d4b7105d754a06374d81259',
+    events: '4d4b7105d754a06373d81259',
+    nightlife: '4d4b7105d754a06376d81259',
+    hotel: '4bf58dd8d48988d1fa931735'
+};
+const url =`https://api.foursquare.com/v2/venues/explore?limit=10&radius=250&`;
+const params = {
+    client_id: 'L3TKFKNJ2AXEJW5L1ILMIBHOMGLSLPE45GK1EI0V05YBSMLX',
+    client_secret: '0CPKJLIZUPM4OHFWTT5UIGMDXTANUCG1NOIR0APWYRYZXZ4D',
+    v: '20180323'
+};
 function formatParams(params) {
-    return Object.keys(params)
-    .map(key => `${key}=${params[key]}`).join('&');
-}
+    return Object.keys(params).map(key => `${key}=${params[key]}`).join('&');
+};
 
 function hideHomePage() {
     $('.homePage').addClass('hidden');
-}
+};
 function displayButtons() {
     $('.results').removeClass('hidden');
     
-}
+};
 function searchAgain() {
     $('#searchAgain').on('click', e => {
         $('.homePage').removeClass('hidden');
@@ -21,7 +32,6 @@ function searchAgain() {
         });
     };
 
-    
 function watchForm() {
     $('form').submit(function(e) {
     e.preventDefault();
@@ -33,8 +43,8 @@ function watchForm() {
     displayButtons();
     searchAgain();
     changeActiveSearch();
-  })
-}
+  });
+};
 
 function displayWeather(response) {
     let location = response['location']['name']+" "+response['location']['region'];
@@ -64,25 +74,9 @@ function searchWeather() {
 };
 
 function searchUrl(){
-    console.log(city);
-    let catagories ={
-        food: '4d4b7105d754a06374d81259',
-        events: '4d4b7105d754a06373d81259',
-        nightlife: '4d4b7105d754a06376d81259',
-        hotel: '4bf58dd8d48988d1fa931735'
-    }
-    let active = document.getElementsByClassName('active')[0].id;
-    console.log(catagories[active]);
-    const url =`https://api.foursquare.com/v2/venues/explore?limit=10&radius=250&near=${city}&`;
-    let params = {
-        client_id: 'L3TKFKNJ2AXEJW5L1ILMIBHOMGLSLPE45GK1EI0V05YBSMLX',
-        client_secret: '0CPKJLIZUPM4OHFWTT5UIGMDXTANUCG1NOIR0APWYRYZXZ4D',
-        v: '20180323',
-        categoryId: catagories[active]
-    };
-    let newUrl = url + formatParams(params); 
-    console.log(newUrl);
-    fetch(newUrl) 
+    params['near']=city;
+    params.categoryId=catagories[document.getElementsByClassName('active')[0].id];
+    fetch(url + formatParams(params)) 
         .then(response => {
             if(response.ok) {
                 return response.json();
@@ -93,19 +87,24 @@ function searchUrl(){
             display(response)
         })
         .catch(function(error) {
-        console.error(error);
+            displayError(error);
         });
 };
+
+function displayError(error) {
+    $('.displayResults').append(
+        `<p>${error}</p>`
+        );
+}
+
 function display(response) {
     console.log(response);
     if(response.response.groups[0].items.length===0) {
         $('.displayResults').append(
         `<p>Sorry no ${document.getElementsByClassName('active')[0].id} listed at this time.</p>`
-        )
+        );
     }
     response.response.groups[0].items.forEach(index =>{
-        // send id for venue 
-        // getVenuePhoto(index.venue.id)
         $('.displayResults').append(
             `<li>
             <pclass='venueName'><img src='${index.venue.categories[0].icon.prefix}bg_32${index.venue.categories[0].icon.suffix}'>${index.venue.name}</p>
@@ -114,22 +113,7 @@ function display(response) {
             );
     });  
 };
-// get venue photo from id
-// function getVenuePhoto(id){
-//     fetch(`https://api.foursquare.com/v2/venues/${id}/photos?limit=1&client_id=L3TKFKNJ2AXEJW5L1ILMIBHOMGLSLPE45GK1EI0V05YBSMLX&client_secret=0CPKJLIZUPM4OHFWTT5UIGMDXTANUCG1NOIR0APWYRYZXZ4D&v=20180323&group=venue`)
-//         .then(response => {
-//             if(response.ok) {
-//                 return response.json();
-//         }
-//             throw new Error(response.statusText);
-//         })
-//         .then(response => {
-//             imgDisplay(response)
-//         })
-//         .catch(function(error) {
-//         console.error(error);
-//         });
-// }
+
 function changeActiveSearch(){
     $('body').on('click', '.venues', function(e) {
         e.preventDefault();
